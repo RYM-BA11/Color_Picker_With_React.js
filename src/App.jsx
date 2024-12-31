48964b39ba343184dce02837fb8b35f51239cfeb
RYM-BA11 <rymbachrouch11m04@gmail.com>
2024-12-31 15:01:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorPicker from './ColorPicker/ColorPicker.jsx';
+
+function App() {
+    return (
+        <>
+            <ColorPicker />
+        </>
+    );
+}
+
+export default App;
